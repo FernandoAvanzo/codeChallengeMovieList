4ddff743fba1bc56ffbce55f87cf478c89655116
favanzo <f.avanzo@cinq.com.br>
2020-07-04 15:51:50 -0300

    Montando Entidades

---
+++ app/src/main/java/code/challenge/movieList/listOfMovies/model/repository/provider/ListOfMoviesService.kt
@@ -0,0 +1,6 @@
+package code.challenge.movieList.listOfMovies.model.repository.provider
+
+import code.challenge.movieList.infrastructure.defaultComponents.model.repository.provider.DefaultServiceAPI
+
+interface ListOfMoviesService: DefaultServiceAPI  {
+}
