4da8a6fb0c5eaff2abc17c0112aadf7815dde6eb
favanzo <f.avanzo@cinq.com.br>
2020-07-04 15:56:47 -0300

    Montando Entidades

--- app/src/main/java/code/challenge/movieList/infrastructure/defaultComponents/model/repository/provider/DefaultProvider.kt
+++ app/src/main/java/code/challenge/movieList/infrastructure/defaultComponents/model/repository/provider/DefaultProvider.kt
@@ -3,10 +3,10 @@ package code.challenge.movieList.infrastructure.defaultComponents.model.reposito
 import android.content.Context
 import code.challenge.movieList.infrastructure.defaultComponents.network.RestProvider
 
-abstract class DefaultProvider<S : DefaultServiceAPI>(context: Context, url: String? = "") :
+abstract class DefaultProvider<T : DefaultServiceAPI>(context: Context, url: String? = "") :
     RestProvider(context, url) {
 
-    val service: S by lazy { retrofit.create(loadServiceClass()) }
+    val service: T by lazy { retrofit.create(loadServiceClass()) }
 
-    abstract fun loadServiceClass(): Class<S>
+    abstract fun loadServiceClass(): Class<T>
 }
